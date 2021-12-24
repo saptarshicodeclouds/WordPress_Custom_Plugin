1bd51722871ff09c0d7cb5b4e48c58010ac2adfb
Saptarshi Sabui <87848468+saptarshicodeclouds@users.noreply.github.com>
2021-12-24 17:37:43 +0530

    first commit

---
+++ includes/class-wcp-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       www.ssdev.com
+ * @since      1.0.0
+ *
+ * @package    Wcp
+ * @subpackage Wcp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wcp
+ * @subpackage Wcp/includes
+ * @author     SSDev <ssdev@wcp.com>
+ */
+class Wcp_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wcp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       www.ssdev.com
+ * @since      1.0.0
+ *
+ * @package    Wcp
+ * @subpackage Wcp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wcp
+ * @subpackage Wcp/includes
+ * @author     SSDev <ssdev@wcp.com>
+ */
+class Wcp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
